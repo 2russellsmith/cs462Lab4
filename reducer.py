e3829b6a177751243659f3910eafbcc92b7b75d7
Russell <2russellsmith@gmail.com>
2016-08-25 11:59:58 -0700

    Moved stopword check to mapper

--- reducer.py
+++ reducer.py
@@ -9,21 +9,6 @@
 from operator import itemgetter
 import sys
 
-stopwords = ['i','me','my','myself','we','our','ours',
-'ourselves','you','your','yours','yourself','yourselves',
-'he','him','his','himself','she','her','hers','herself',
-'it','its','itself','they','them','their','theirs','themselves',
-'what','which','who','whom','this','that','these','those','am','is',
-'are','was','were','be','been','being','have','has','had','having','do',
-'does','did','doing','a','an','the','and','but','if','or','because','as',
-'until','while','of','at','by','for','with','about','against','between',
-'into','through','during','before','after','above','below','to','from','up',
-'down','in','out','on','off','over','under','again','further','then','once',
-'here','there','when','where','why','how','all','any','both','each','few','more','most',
-'other','some','such','no','nor','not','only','own','same','so','than','too','very','s',
-'t','can','will','just','don','should','now'
-]
-
 # stopwords = []
 
 # maps words to their counts
@@ -38,9 +23,8 @@ for line in sys.stdin:
 	word, count = line.split('\t', 1)
 	# convert count (currently a string) to int
 	try:
-		if(word not in stopwords):
-		    count = int(count)
-		    word2count[word] = word2count.get(word, 0) + count
+		count = int(count)
+		word2count[word] = word2count.get(word, 0) + count
 	except ValueError:
 		# count was not a number, so silently
 		# ignore/discard this line
